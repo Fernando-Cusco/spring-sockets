ef7b2b016b1bf160187cb80617c278a6ab800f6b
Fernando Cusco <bcuscom@est.ups.edu.ec>
2021-08-19 12:21:12 -0500

    websockets

---
+++ src/main/java/ec/edu/ups/messagingwebsocket/HelloMessage.java
@@ -0,0 +1,17 @@
+package ec.edu.ups.messagingwebsocket;
+
+public class HelloMessage {
+
+    private String name;
+
+    public HelloMessage() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/ec/edu/ups/messagingwebsocket/MessagingWebsocketApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.ups.messagingwebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingWebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagingWebsocketApplication.class, args);
+	}
+
+}
